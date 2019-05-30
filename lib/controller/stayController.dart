f730b232dc3fda4f2a6c884bdfafa917bf4a39b6
Jonas Schütz <jonas.schuetz@students.fhnw.ch>
2019-05-30 16:27:44 +0200

    StayController works!!

--- lib/controller/stayController.dart
+++ lib/controller/stayController.dart
@@ -1,41 +1,62 @@
+import 'dart:convert';
+
+
 import 'package:radon_http/model/stay.dart';
 import 'package:radon_http/radon_http.dart';
 
-class StayController extends ResourceController{
+
+class StayController extends ResourceController {
   StayController(this.context);
 
   final ManagedContext context;
 
+  @override
+  List<ContentType> acceptedContentTypes = [
+    ContentType("application", "x-www-form-urlencoded")];
+
   @Operation.get()
-  Future<Response> getAllStays() async{
+  Future<Response> getAllStays() async {
     final stayQuery = Query<Stay>(context);
     final stays = await stayQuery.fetch();
 
     return Response.ok(stays);
   }
 
-  @Operation.get('id') 
+  @Operation.get('id')
   Future <Response> getStaysByID(@Bind.path('id') int id) async {
     final stayQuery = Query<Stay>(context)
-    ..where((s) => s.id).equalTo(id);
+      ..where((s) => s.id).equalTo(id);
 
     final stay = await stayQuery.fetch();
-    if(stay == null){
+    if (stay == null) {
       return Response.notFound();
     }
     return Response.ok(stay);
   }
+
+
   @Operation.post()
   Future<Response> createStay() async {
-    final map = await request.body.decode<Map<String, String>>();
+    logger.parent.level = Level.INFO;
+    logger.onRecord.listen((rec) =>
+        print("$rec ${rec.error ?? ""} ${rec.stackTrace ?? ""}"));
+
+
+    final Map<String, dynamic> json = await request.body.decode();
     final query = Query<Stay>(context)
-    ..values.id = int.parse(map['id'])
-    ..values.startTime = DateTime.parse(map['startTime'])
-    ..values.endTime = DateTime.parse(map['endTime'])
-    ..values.dose = int.parse(map['dose']);
+      ..values.id = int.parse(json["id"].first.toString())
+      ..values.startTime = DateTime.now() //json["startTime"] == null
+//    ? null
+//    : DateTime.parse(json["startTime"].first.toString() )
+      ..values.endTime = DateTime.now() //json["endTime"] == null
+//    ? null
+//        : DateTime.parse(json["endTime"].first.toString())
+      ..values.dose = int.parse(json["dose"].first.toString());
 
-    final insertedStay = await query.insert();
 
-    return Response.ok(insertedStay);
+    final insertedStay = query.insert();
+    var ok = 'ok';
+
+    return Response.ok(ok);
   }
 }
--- lib/model/stay.dart
+++ lib/model/stay.dart
@@ -1,30 +1,20 @@
 import 'package:radon_http/radon_http.dart';
 
+
+
 class Stay extends ManagedObject<_Stay> implements _Stay {
 
-  _Stay create(Map<String, String> json) {
-    final stay =  _Stay.fromJson(json);
-    return stay;
-  }
 }
 
 class _Stay {
 
-
-  _Stay.fromJson(Map<String, String> json)
-      : id = int.parse(json['id']),
-        startTime = DateTime.parse(json['startTime']),
-        endTime = DateTime.parse(json['endTime']),
-        dose = int.parse(json['dose']);
-
   @primaryKey
   int id;
-  @Column(unique: false, nullable: false)
+  @Column(unique: false, nullable: true)
   DateTime startTime;
-  @Column(unique: false, nullable: false)
+  @Column(unique: false, nullable: true)
   DateTime endTime;
-  @Column(unique: false, nullable: false)
+  @Column(unique: false, nullable: true)
   int dose;
-
 
 }
